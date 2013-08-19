b943fe95e3293cfc57dc7270ecf15cfc4e385c9d
linyows <linyows@gmail.com>
2013-08-19 15:10:51 +0900

    bump version to 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'linyows@gmail.com'
 license          'MIT'
 description      'Installs and manages your versions of node.js in chef with nodebrew'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 recipe "nodebrew", "Installs and configures nodebrew"
 
