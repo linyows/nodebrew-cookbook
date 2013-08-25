a59512badd1b3f11a28102fc5ebe0a52e1de5380
linyows <linyows@gmail.com>
2013-08-26 00:41:22 +0900

    support why-run

--- providers/node.rb
+++ providers/node.rb
@@ -3,46 +3,54 @@
 
 include Chef::Mixin::Nodebrew
 
+def whyrun_supported?
+  true
+end
+
 action :install do
-  case
-  when node_installed?(new_resource.version)
-    message_when_the_installed(new_resource)
+  converge_by "Install #{new_resource}" do
+    case
+    when node_installed?(new_resource.version)
+      message_when_the_installed(new_resource)
 
-  else
-    Chef::Log.info "Building #{new_resource}, this could take a while..."
-    install_start = Time.now
+    else
+      Chef::Log.info "Building #{new_resource}, this could take a while..."
+      install_start = Time.now
 
-    install = 'install'
-    install << '-binary' if new_resource.binary
-    command = "nodebrew #{install} #{new_resource.version}"
+      install = 'install'
+      install << '-binary' if new_resource.binary
+      command = "nodebrew #{install} #{new_resource.version}"
 
-    nodebrew_script command do
-      action :nothing
-      code command
-    end.run_action(:run)
+      nodebrew_script command do
+        action :nothing
+        code command
+      end.run_action(:run)
 
-    install_time = (Time.now - install_start) / 60.0
-    Chef::Log.info "#{new_resource} build time was #{install_time} minutes"
+      install_time = (Time.now - install_start) / 60.0
+      Chef::Log.info "#{new_resource} build time was #{install_time} minutes"
 
-    new_resource.updated_by_last_action(true)
+      new_resource.updated_by_last_action(true)
+    end
   end
 end
 
 action :uninstall do
-  case
-  when !node_installed?(new_resource.version)
-    message_when_the_not_installed(new_resource)
+  converge_by "Uninstall #{new_resource}" do
+    case
+    when !node_installed?(new_resource.version)
+      message_when_the_not_installed(new_resource)
 
-  else
-    command = "nodebrew uninstall #{new_resource.version}"
+    else
+      command = "nodebrew uninstall #{new_resource.version}"
 
-    nodebrew_script command do
-      action :nothing
-      code command
-    end.run_action(:run)
+      nodebrew_script command do
+        action :nothing
+        code command
+      end.run_action(:run)
 
-    Chef::Log.info "#{new_resource} was successfully uninstall"
+      Chef::Log.info "#{new_resource} was successfully uninstall"
 
-    new_resource.updated_by_last_action(true)
+      new_resource.updated_by_last_action(true)
+    end
   end
 end
